39d13688d80da054b98b6c051412835b6aa59488
Caroline Marsden <car.marsden@gmail.com>
2019-06-17 19:53:12 -0400

    Add Currency error boundaries exercise

--- react-playground/src/index.js
+++ react-playground/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
+// import App from './playingwithdanger/App';
+import AppLang from './lang-context/AppLang';
 import './index.css';
 
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <AppLang />
     </BrowserRouter>,
     document.getElementById('root')
 );
